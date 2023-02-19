d594707ad4150d679e6f5fce3542c6cd4cd03fd3
Corey <75544957+TheHamhams@users.noreply.github.com>
2023-02-19 11:53:29 -0800

    initial

---
+++ JwtNetBlog/src/Components/NavBar/NavBar.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import Navbar from 'react-bootstrap/Navbar'
+import Nav from 'react-bootstrap/Nav'
+import Container from 'react-bootstrap/Container'
+
+export const NavBar = () => {
+  return (
+    <Navbar bg='dark' variant='dark' className='fixed-top navbar'>
+        <Container>
+            <Navbar.Brand>NetBlog</Navbar.Brand>
+            <Nav className='ms-auto'>
+                <Nav.Link href="/">Home</Nav.Link>
+            </Nav>
+        </Container>
+    </Navbar>
+  )
+}
